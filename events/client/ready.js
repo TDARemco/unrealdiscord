d2d9a3eb22bede8bb19aad8088d4ae88c1aa40da
solaresports <70237441+solaresports@users.noreply.github.com>
2021-01-04 16:24:09 +0100

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,14 +1,14 @@
 module.exports = client => {
-    console.log("[AvocBE] De bot is aan het opstarten");
-    console.log("[AvocBE] De data is ingeladen");
-    console.log("[AvocBE] Plugin date: 5%");
-    console.log("[AvocBE] Plugin date: 19%");
-    console.log("[AvocBE] Plugin date: 26%");
-    console.log("[AvocBE] Plugin date: 39%");
-    console.log("[AvocBE] Plugin date: 51%");
-    console.log("[AvocBE] Plugin date: 78%");
-    console.log("[AvocBE] Plugin date: 92%");
-    console.log("[AvocBE] Plugin date: 100%");
-    console.log("[AvodBE] De bot is volledig opgestart");
+    console.log("[Unreal] De bot is aan het opstarten");
+    console.log("[Unreal] De data is ingeladen");
+    console.log("[Unreal] Plugin date: 5%");
+    console.log("[Unreal] Plugin date: 19%");
+    console.log("[Unreal] Plugin date: 26%");
+    console.log("[Unreal] Plugin date: 39%");
+    console.log("[Unreal] Plugin date: 51%");
+    console.log("[Unreal] Plugin date: 78%");
+    console.log("[Unreal] Plugin date: 92%");
+    console.log("[Unreal] Plugin date: 100%");
+    console.log("[Unreal] De bot is volledig opgestart");
 
 }
