f98d579eac06ca189cc880cfa1b6ed102d397e7f
solaresports <70237441+solaresports@users.noreply.github.com>
2021-01-04 16:41:45 +0100

    Update SRI1.js

--- commands/info/SRI1.js
+++ commands/info/SRI1.js
@@ -13,18 +13,21 @@ module.exports = {
             **Server IP:**
             Etalive.mygs.co
 
-            **Het Concept**+
+            **Het Concept**
+
             Een friendly whitelisted survival wereld.
             Vanilla minecraft.
             Iedereen begint op de coördinaten X: 0 Y: 74 Z: 0 (je spawned hier vanzelf)
             Alles word vastgelegd in de logs dus stelen/greeven is een no go 
 
             **Trade Zone:**
+
             De trade zone is een zone waar je gebouwen mag maken die alleen bedoeld zijn voor het traden van spullen.
             Er is geen in game currency dus je zult moeten traden met spullen net zoals vroeger gebeurde.
             De trade zone is een zone van 1000 blokken vanaf nul
 
             **De Map**
+            
             We hebben een live map waar je elkaar kan zien.
             Seed: 8426026102288270951
             http://51.195.33.175:8123/`)
