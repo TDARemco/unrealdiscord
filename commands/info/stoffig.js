3ec9a30ab8e4511b8c722ed4d68f359d2d71267a
solaresports <70237441+solaresports@users.noreply.github.com>
2021-01-05 17:59:21 +0100

    Update stoffig.js

--- commands/info/stoffig.js
+++ commands/info/stoffig.js
@@ -11,14 +11,14 @@ module.exports = {
             .setTitle("GRATIS RECLAME")
             .setDescription(`
             Hallo, Dit is gratis reclame, gratis is goed! Toch?
-            Nou, ik zou zeggen ga lekker naar mijn twitch en help me naar de 1000 follows!
+            Nou, ik zou zeggen ga lekker naar mijn twitch, 
+            en help me naar de 1000 follows!
             Dat is cool, en cool is ook goed!
-            
+
             https://www.twitch.tv/stoffigkontje`)
             .setColor("#f2495f")
             .setImage("https://cdn.discordapp.com/attachments/783453881828900964/796059486506582056/subbadgecropped.png")
-            .setThumbnail("https://cdn.discordapp.com/attachments/783453881828900964/796059486506582056/subbadgecropped.png")
-
+            
         message.delete().catch(err => console.log(err));
         return message.channel.send(botEmbed);
     }
