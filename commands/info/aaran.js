772794ea1f69f2e98023b41d88d15de6b245157f
solaresports <70237441+solaresports@users.noreply.github.com>
2021-01-05 17:53:06 +0100

    Update aaran.js

--- commands/info/aaran.js
+++ commands/info/aaran.js
@@ -13,6 +13,8 @@ module.exports = {
             Aaran is een aaran, een aaran is een aaran.
             Vraag nie waar je er een vind, je moet zelf zoeken!`)
             .setColor("#4EF0E8")
+            .setImage("https://cdn.discordapp.com/attachments/779169509671829546/796058461992386611/aaran.png")
+            .setThumbnail("https://cdn.discordapp.com/attachments/779169509671829546/796058461992386611/aaran.png")
 
         message.delete().catch(err => console.log(err));
         return message.channel.send(botEmbed);
